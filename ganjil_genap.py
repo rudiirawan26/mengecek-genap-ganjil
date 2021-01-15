a63625ca5db1db7452dcec3b44f81eb7ea39620c
RUDI IRAWAN <50778467+rudiirawan26@users.noreply.github.com>
2021-01-15 20:03:14 +0700

    Create ganjil_genap.py

---
+++ ganjil_genap.py
@@ -0,0 +1,9 @@
+# # Contoh output:
+# Anda memasukkan bilangan 10, 10 adalah bilangan genap
+
+
+num = int(input("Masukan Sebuah bilangan: "))
+if num % 2 == 0:
+    print(f"nda memasukkan bilangan {num}, {num} adalah bilangan genap ")
+else:
+    print(f"nda memasukkan bilangan {num}, {num} adalah bilangan ganjil ")
